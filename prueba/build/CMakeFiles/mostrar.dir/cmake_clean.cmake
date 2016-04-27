4bdc8f7f2329d5d14d8bf9a4a3eb0634ca569fd7
kennyBedoya <kebedoya@utp.edu.co>
2016-04-27 14:58:24 -0500

    archivos de mostrar imagen

---
+++ prueba/build/CMakeFiles/mostrar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mostrar.dir/mostrar.cc.o"
+  "mostrar.pdb"
+  "mostrar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mostrar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
