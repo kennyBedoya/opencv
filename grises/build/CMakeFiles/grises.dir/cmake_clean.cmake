99c534a3fd0d78335db06909b47ff0187df5f6f9
anaguicas <angelicaaguicas@gmail.com>
2016-04-27 15:30:53 -0500

    algoritmo escala grises

---
+++ grises/build/CMakeFiles/grises.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grises.dir/grises.cc.o"
+  "grises.pdb"
+  "grises"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
