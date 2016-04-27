c65751a359d9dfcffe3f255894128a98937e5d9d
kennyBedoya <kebedoya@utp.edu.co>
2016-04-27 14:51:16 -0500

    prueba de mostrar imagen

---
+++ prueba/mostrar.cc
@@ -0,0 +1,28 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{
+  if( argc != 2) {
+    cout <<" Usage: mostrar_imagen <archivo de imagen>" << endl;
+    return -1;
+  }
+
+  Mat image;
+  image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+  if(! image.data ) {
+    cout <<  "Could not open or find the image" << std::endl ;
+    return -1;
+}
+
+  namedWindow( "Display window", CV_WINDOW_AUTOSIZE );
+  imshow( "Display window", image );
+
+  waitKey(0);
+  return 0;
+}
