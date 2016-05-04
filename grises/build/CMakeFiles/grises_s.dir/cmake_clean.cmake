4f449c72d55cc323bd2a59568394ccb371ac4d82
anaguicas <angelicaaguicas@gmail.com>
2016-05-04 17:15:50 -0500

    escala a grises secuencial, con error

---
+++ grises/build/CMakeFiles/grises_s.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grises_s.dir/grises.cc.o"
+  "grises_s.pdb"
+  "grises_s"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grises_s.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
