cf82e3ecd4c5a6c631c06f54fce2bbdd24ab2cd6
kenny <kebedoya@utp.edu.co>
2016-05-24 17:33:56 -0500

    sobel cache

---
+++ grises_cache/build/CMakeFiles/cache.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cache.dir/./cache_generated_cache.cu.o"
+  "cache.pdb"
+  "cache"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/cache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ grises_global/build/CMakeFiles/global.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/global.dir/./global_generated_global.cu.o"
+  "global.pdb"
+  "global"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/global.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
